e6589bfa71aacb48281614ac35056faad4fda203
asifkhanmzn8218-ai <asifkhan.mzn8218@gmail.com>
2026-09-06 23:40:26 +0530

    Set base path and format plugins in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,13 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: "/DCCHunter/",
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile()
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
